363e67576a673290681dc6cee657ead9e86ee01a
ak <anandakannan003@gmail.com>
2024-09-25 23:31:58 +0530

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,99 @@
+export const data = [
+    {
+        question: "Which HTML element is used to define the largest heading?",
+        option1: "<h1>",
+        option2: "<h3>",
+        option3: "<h5>",
+        option4: "<p>",
+        ans: 1,
+      },
+      {
+        question: "Which property is used in CSS to change the background color?",
+        option1: "background-color",
+        option2: "color",
+        option3: "bgcolor",
+        option4: "background",
+        ans: 1,
+      },
+      {
+        question: "Which JavaScript method is used to select an element by its ID?",
+        option1: "getElementByClass",
+        option2: "getElementById",
+        option3: "querySelector",
+        option4: "getElementByTag",
+        ans: 2,
+      },
+      {
+        question: "Which HTTP method is used to submit data to be processed to a server?",
+        option1: "GET",
+        option2: "POST",
+        option3: "DELETE",
+        option4: "PATCH",
+        ans: 2,
+      },
+      {
+        question: "Which tag is used to include an external JavaScript file in HTML?",
+        option1: "<link>",
+        option2: "<style>",
+        option3: "<script>",
+        option4: "<meta>",
+        ans: 3,
+      },
+      {
+        question: "Which of the following is a frontend JavaScript framework?",
+        option1: "Node.js",
+        option2: "Django",
+        option3: "React.js",
+        option4: "Laravel",
+        ans: 3,
+      },
+      {
+        question: "What does CSS stand for?",
+        option1: "Creative Style Sheets",
+        option2: "Cascading Style Sheets",
+        option3: "Computer Style Sheets",
+        option4: "Colorful Style Sheets",
+        ans: 2,
+      },
+      {
+        question: "Which of the following is a NoSQL database?",
+        option1: "MySQL",
+        option2: "PostgreSQL",
+        option3: "MongoDB",
+        option4: "SQLite",
+        ans: 3,
+      },
+      {
+        question: "In React, which hook is used to handle side effects?",
+        option1: "useEffect",
+        option2: "useState",
+        option3: "useReducer",
+        option4: "useContext",
+        ans: 1,
+      },
+      {
+        question: "What is the default port number for HTTP?",
+        option1: "80",
+        option2: "8080",
+        option3: "443",
+        option4: "21",
+        ans: 1,
+      }
+    ];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
